6c2a835d6cc39299d708e4b41b708c4fd29c52c6
SaiPrakashSarisa <109500075+SaiPrakashSarisa@users.noreply.github.com>
2023-05-31 05:30:40 -0700

    1 binaries uploaded

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+
+interface CreditCard {
+  cardnumber: number;
+  card1: number;
+  card2: number;
+  card3: number;
+  card4: number;
+  expdate: string;
+  type: string;
+  cvv: number;
+}
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.css']
+  styleUrls: ['./card.component.css'],
 })
 export class CardComponent implements OnInit {
+  @Input()
+  creditCard!: CreditCard;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
