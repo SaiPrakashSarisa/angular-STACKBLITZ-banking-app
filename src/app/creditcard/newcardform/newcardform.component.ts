25d41c3d4ffab8938de5d382086168538a2b4dc5
SaiPrakashSarisa <109500075+SaiPrakashSarisa@users.noreply.github.com>
2023-06-01 08:17:54 -0700

    3 files modified

--- src/app/creditcard/newcardform/newcardform.component.ts
+++ src/app/creditcard/newcardform/newcardform.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-newcardform',
   templateUrl: './newcardform.component.html',
-  styleUrls: ['./newcardform.component.css']
+  styleUrls: ['./newcardform.component.css'],
 })
 export class NewcardformComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
-  }
-
+  newCard = new FormGroup({
+    cardNumber: new FormControl(''),
+    cvv: new FormControl(''),
+    cardType: new FormControl(''),
+  });
 }
